13b4e06a94913783554f545d3708a5051cddf9ff
Thirupathi Reddy <ttreddysmail@gmail.com>
2025-09-22 21:19:11 +0530

    read command example

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "please enter your PIN nubmer"
-read PIN_NUMBER # here pin nubmer is VARIABLE
+read -s PIN_NUMBER # here pin nubmer is VARIABLE
 echo "PIN entered is : $PIN_NUMBER"

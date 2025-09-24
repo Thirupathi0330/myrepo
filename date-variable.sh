125b564aed323bd2f53f0257352a0347dbe26acb
Thirupathi Reddy <ttreddysmail@gmail.com>
2025-09-24 21:10:44 +0530

    some message

--- date-variable.sh
+++ date-variable.sh
@@ -3,5 +3,5 @@
 START_TIME=$(date +%s)
 sleep 10
 END_TIME=$(date +%s)
-TOTAL TIME=(($END_TIME-$START_TIME))
+TOTAL_TIME=(($END_TIME-$START_TIME))
 echo "Total time executed : $TOTAL_TIME in minutes"
